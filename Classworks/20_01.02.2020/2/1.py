7fa8c0652ac4a8adc6b90e334de92f69c7197149
waldis_jr <waldis_jr@outlook.com>
2020-05-06 17:38:29 +0300

    Merge branch 'master' of https://github.com/waldis-jr-dev/Python_private
    # Conflicts:
    #	Packeges/my_data.py

--- Classworks/20_01.02.2020/2/1.py
+++ Classworks/20_01.02.2020/2/1.py
@@ -1,11 +1,11 @@
-import module
+from Private.Packeges import db_config
 
 up_1 = int(input('Entyer up_1: '))
 down_1 = int(input('Entyer down_1: '))
 up_2 = int(input('Entyer up_2: '))
 down_2 = int(input('Entyer down_2: '))
 
-print(f"Addition = {module.addition(up_1, down_1, up_2, down_2)}")
-print(f"Subtraction = {module.subtraction(up_1, down_1, up_2, down_2)}")
-print(f"Multiplication = {module.multiplication(up_1, down_1, up_2, down_2)}")
-print(f"Division = {module.division(up_1, down_1, up_2, down_2)}")
+print(f"Addition = {db_config.addition(up_1, down_1, up_2, down_2)}")
+print(f"Subtraction = {db_config.subtraction(up_1, down_1, up_2, down_2)}")
+print(f"Multiplication = {db_config.multiplication(up_1, down_1, up_2, down_2)}")
+print(f"Division = {db_config.division(up_1, down_1, up_2, down_2)}")
