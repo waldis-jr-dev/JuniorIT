c4b1f1e7fc522db45edb2e10e6b68288113c3759
Waldis Jr <waldis_jr@outlook.com>
2020-09-19 00:01:23 +0300

    hw on 19.09.2020

--- 2020-2021/Homeworks/2.Homework_on_19.09.2020/1.py
+++ 2020-2021/Homeworks/2.Homework_on_19.09.2020/1.py
@@ -3,25 +3,25 @@ from typing import Generator, Optional
 
 def lines() -> Generator[Optional[str], Optional[str], None]:
     active_files = {}
-    name = 'default'
+    name = ''
     resp = yield None
-    if resp != '':
-        name = resp
     while True:
         while True:
             try:
+                if resp not in ['', None]:
+                    name = resp
                 with open('files/' + name + '.txt', 'r') as file:
                     data = file.readlines()
                     break
             except FileNotFoundError:
-                name = yield 'File not found, try another !'
+                resp = yield 'File not found, try another !'
         if name not in active_files.keys():
             target = range(len(data))
         else:
             target = range(active_files[name] + 1, len(data))
         for number_of_line in target:
             resp = yield data[number_of_line].strip()
-            if resp != '':
+            if resp not in ['', None]:
                 active_files[name] = number_of_line
                 name = resp
                 break
@@ -33,5 +33,6 @@ if __name__ == '__main__':
     gen = lines()
     next(gen)
     while True:
-        answer = input('Name / (enter to continue) : ')
+        answer = input('File name / (enter to continue) : ').strip()
         print(gen.send(answer))
+        # print(next(gen))
