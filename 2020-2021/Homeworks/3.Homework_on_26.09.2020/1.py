d4ac3fa564a082141bafd5c241fadcce8ffe46d2
Waldis Jr <waldis_jr@outlook.com>
2020-09-20 22:39:23 +0300

    hw_on_26.09.2020

--- 2020-2021/Homeworks/3.Homework_on_26.09.2020/1.py
+++ 2020-2021/Homeworks/3.Homework_on_26.09.2020/1.py
@@ -1,3 +1,3 @@
 a = list(range(-1000, 0))
-a = [i**2 - 2*len(str(i)) for i in a if str(i)[-2] != '7' and sum(map(int, str(i)[1::])) >= i]
+a = [i**2 - 2*len(str(i)) for i in a if str(i)[-2] != '7' and sum(map(int, str(i)[1::])) >= int(str(i)[1::])]
 print(a)
