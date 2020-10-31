5f5e0bf35c7f7e5dd40e07175f1675b2132f7351
Waldis Jr <waldis_jr@outlook.com>
2020-10-31 13:24:46 +0200

    31.10.2020

--- _2020_2021/Homeworks/_8_hw_on_31_10_2020/_1.py
+++ _2020_2021/Homeworks/_8_hw_on_31_10_2020/_1.py
@@ -1,4 +1,4 @@
-from _2020_2021.Classworks._8_24_10_2020._2 import Passenger, Person
+from _2020_2021.Classworks._8_24_10_2020._2 import Passenger
 import sqlite3
 
 conn = sqlite3.connect('customers.sqlite3')
@@ -6,10 +6,10 @@ cursor = conn.cursor()
 
 
 class Customer:
-    @staticmethod
-    def find_customer(id):
+    @classmethod
+    def find_customer(cls, u_id):
         sql = f"SELECT * FROM customers WHERE id LIKE ?"
-        data = cursor.execute(sql, (id,)).fetchall()
+        data = cursor.execute(sql, (u_id,)).fetchall()
         if len(data) == 0:
             return 'No customer with this ID !'
         else:
@@ -17,14 +17,14 @@ class Customer:
         customer = Passenger()
         customer.insert_data(id=data[0],
                              name=data[1],
-                             birth_year= data[2],
+                             birth_year=data[2],
                              start_location=data[3],
                              end_location=data[4],
                              distance=data[5])
         return customer
 
-    @staticmethod
-    def insert_cost(customer: Passenger):
+    @classmethod
+    def insert_cost(cls, customer: Passenger):
         sql = '''INSERT INTO cost (customer_id, cost) VALUES (?,?)'''
         val = (customer.id, customer.price_for_km())
         cursor.execute(sql, val)
@@ -34,7 +34,7 @@ class Customer:
 if __name__ == '__main__':
     test = Customer()
 
-    customer1 = test.find_customer(2)
+    customer1 = test.find_customer(1)
     print(customer1)
     test.insert_cost(customer1)
 
