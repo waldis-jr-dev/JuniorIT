cde6a0532da88ab9f85406b3e6ec8669d7b93c82
Waldis Jr <waldis_jr@outlook.com>
2020-12-19 10:10:58 +0200

    Test 1

--- _2020_2021/Exams/Test_1/_1.py
+++ _2020_2021/Exams/Test_1/_1.py
@@ -1,24 +1,29 @@
 from abc import ABC, abstractmethod
+from typing import Tuple, Union
 
 
 class AbstractNumber(ABC):
+    @abstractmethod
     def __add__(self, other):
         pass
 
+    @abstractmethod
     def __sub__(self, other):
         pass
 
+    @abstractmethod
     def __mul__(self, other):
         pass
 
+    @abstractmethod
     def __truediv__(self, other):
         pass
 
 
 class Number(AbstractNumber):
-    def __init__(self, whole_part: str, fraction_part: str):
-        self.whole_part = whole_part
-        self.fraction_part = fraction_part
+    def __init__(self, whole_part: Union[str, int], fraction_part: Union[int, str]):
+        self.whole_part = str(whole_part)
+        self.fraction_part = str(fraction_part)
 
     def __add__(self, other):
         first, second = self.add_0(self.fraction_part, other.fraction_part)
@@ -31,8 +36,8 @@ class Number(AbstractNumber):
         first, second = self.add_0(self.fraction_part, other.fraction_part)
         fraction_part = first - second
         if fraction_part < 0:
-            return Number(self.whole_part - other.whole_part - 1, first - fraction_part)
-        return Number(self.whole_part - other.whole_part, fraction_part)
+            return Number(int(self.whole_part) - other.whole_part - 1, first - fraction_part)
+        return Number(int(self.whole_part) - other.whole_part, fraction_part)
 
     def __mul__(self, other):
         all_together = (self.whole_part + self.fraction_part) * (other.whole_part + other.fraction_part)
@@ -40,9 +45,12 @@ class Number(AbstractNumber):
         return Number(all_together[:f:], all_together[f::])
 
     def __truediv__(self, other):
+        all_together = (self.whole_part + self.fraction_part) * (other.whole_part + other.fraction_part)
+        f = len(str(self.fraction_part)) + len(str(other.fraction_part))
+        return Number(all_together[:f:], all_together[f::])
 
     @staticmethod
-    def add_0(first , second) -> int:
+    def add_0(first, second) -> Tuple[int, int]:
         if len(first) == len(second):
             return int(first.lstrip('0')), int(second.lstrip('0'))
         if len(first) > len(second):
@@ -51,6 +59,9 @@ class Number(AbstractNumber):
         if len(first) < len(second):
             first += '0' * (len(second) - len(first))
             return int(first.lstrip('0')), int(second.lstrip('0'))
+
+
+
 
 
 
