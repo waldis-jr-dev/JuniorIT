6a67e713708622ae13db6d17b48e43e3d10611f2
Grynovetskyy Vladyslav <grynovetskyy@gmail.com>
2021-03-27 08:27:34 +0200

    hw_on_27_03_2021

--- _2020_2021/Homeworks/_27_hw_on_27_03_2021/reverse_array.js
+++ _2020_2021/Homeworks/_27_hw_on_27_03_2021/reverse_array.js
@@ -1,6 +1,13 @@
-array1 = ['Python', 'is', 'better', 'and', 'smarter']
-
+array1 = ['Python', 'is', 'better', 'and', 'smarter', 10011]
+reversed_array = []
 let i;
 for (i in array1){
-    console.log(array1[i].split("").reverse().join(""));
+    let element = array1[i]
+    if (isNaN(element)) {
+        reversed_array.push(element.split("").reverse().join(""))
+    }
+    else{
+        reversed_array.push(Number(element.toString().split("").reverse().join("")))
+    }
 };
+console.log(reversed_array)
